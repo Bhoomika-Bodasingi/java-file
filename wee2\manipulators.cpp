4db3dc5ea97a28096a4ec77f6e7c62574ac3afad
Bhoomika-Bodasingi <160243644+Bhoomika-Bodasingi@users.noreply.github.com>
2024-02-16 20:04:36 +0530

    Update wee2\manipulators.cpp

--- wee2\manipulators.cpp
+++ wee2\manipulators.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+
+    cout<<"My name is Bhanuprakash"<<endl;
+
+    cout<<"Hi"<<ends;
+    cout<<endl;
+    string name;
+    getline(cin>>ws,name);
+    cout<<name<<endl;
+    cout<<setw(5)<<"Hi\n";
+    cout<<setfill('^')<<setw(10)<<"Hi maga\n";
+    double num=0.42321678231;
+    cout<<setprecision(3)<<num<<endl;
+    int a=5;
+    cout.flush();
+    cout<<"The value of a is "<<a;
+    return 0;
+}
+
 
