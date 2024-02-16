146862edfcdb0f589da61d920ecc1e9193bf12b2
Bhoomika-Bodasingi <160243644+Bhoomika-Bodasingi@users.noreply.github.com>
2024-02-16 20:08:32 +0530

    Update week2\evenorodd.cpp

--- week2\evenorodd.cpp
+++ week2\evenorodd.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
+void find(int a);
+int main()
+{
+    int a;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    find(a);
+    return 0;
+}
+void find(int a)
+{
+     if(a%2==0)
+    {
+        cout<<a<<" is even\n";
+    }
+    else
+    {
+        cout<<a<<" is odd\n";
+    }
+}
+
 
