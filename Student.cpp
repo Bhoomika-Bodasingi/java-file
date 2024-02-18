5ad7a1b7316b058c24e541c21024b978bdfc0dbb
Bhoomika-Bodasingi <160243644+Bhoomika-Bodasingi@users.noreply.github.com>
2024-02-18 18:23:10 +0530

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+    public:
+    string fullName;
+    int rollNum;
+    double semPerentage;
+    string collegeName;
+    int collegeCode;
+    Student()
+    {
+    cout<<"Student class initialized!!\nIam a student\n";
+    }
+    ~Student(){}
+};
+int main()
+{
+    Student bhoomi;
+    cout<<"Enter your name : ";
+    cin>>bhoomi.fullName;
+    cout<<"Enter your roll number : ";
+    cin>>bhoomi.rollNum;
+    cout<<"Enter your sem percentage : ";
+    cin>>bhoomi.semPerentage;
+    cout<<"Enter your college name : ";
+    cin>>bhoomi.collegeName;
+    cout<<"Enter your college code : ";
+    cin>>bhoomi.collegeCode;
+    cout<<"Name : "<<bhoomi.fullName<<endl;
+    cout<<"RollNum : "<<bhoomi.rollNum<<endl;
+    cout<<"SemPercentage : "<<bhoomi.semPerentage<<endl;
+    cout<<"CollegeName : "<<bhoomi.collegeName<<endl;
+    cout<<"CollegeCode : "<<bhoomi.collegeCode<<endl;
+    return 0;
+}
+
+
 
