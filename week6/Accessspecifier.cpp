94273728bff42dde5b4c6116ed80aab35b9feb13
Bhoomika-Bodasingi <160243644+Bhoomika-Bodasingi@users.noreply.github.com>
2024-03-07 11:17:35 +0530

    Update Accessspecifier.cpp

--- week6/Accessspecifier.cpp
+++ week6/Accessspecifier.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+class AccessSpecifierDemo{
+    private:
+    int priVar;
+    protected:
+    int proVar;
+    public:
+    int pubVar;
+    void setVar(int priValue ,int proValue ,int pubValue)
+    {
+        priVar=priValue;
+        proVar=proValue;
+        pubVar=pubValue;
 
+    }
+    void getVar(){
+        cout<<"private : " <<priVar<<endl;
+        cout<<"protected : " <<proVar<<endl;
+        cout<<"public : " <<pubVar<<endl;
+
+    }
+};
+int main(){
+    AccessSpecifierDemo Demoobj;
+    Demoobj.setVar(10,30,40);
+    Demoobj.getVar();
+    return 0;
+
+}
