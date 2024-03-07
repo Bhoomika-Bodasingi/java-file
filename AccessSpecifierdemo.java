0cd345a980b74aefaf38246162a6bc1e04b8d72a
Bhoomika-Bodasingi <160243644+Bhoomika-Bodasingi@users.noreply.github.com>
2024-03-07 11:42:23 +0530

    Update AccessSpecifierdemo.java

--- AccessSpecifierdemo.java
+++ AccessSpecifierdemo.java
@@ -1,1 +1,37 @@
+class AccessSpecifier{
+    private int priVar;
+    protected int proVar;
+    public int pubVar;
+    public void setVar(int priValue ,int proValue ,int pubValue)
+    {
+        priVar=priValue;
+        proVar=proValue;
+        pubVar=pubValue;
+
+    }
+    public void getVar(){
+        System.out.println("private : " +priVar);
+        System.out.println("protected : " +proVar);
+        System.out.println("public : " +pubVar);
+
+
+    }
+}
+
+public class AccessSpecifierDemo{ 
+    public static void main(String[] args){
+        AccessSpecifier Demoobj=new AccessSpecifier();
+        Demoobj.setVar(10,30,40);
+        Demoobj.getVar();
+
+    }
+
+}
+
+    
+    
+
+
+
+
 
